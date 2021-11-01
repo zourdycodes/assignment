9f58883fbcffc037b9c4b83a16fa600e2b4c5fb4
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-11-01 11:51:07 +0000

    changing documentation

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -53,7 +53,7 @@ MyDocument.getInitialProps = async (ctx) => {
 
   const originalRenderPage = ctx.renderPage;
 
-  // You can consider sharing the same emotion cache between all the SSR requests to speed up performance.
+  // it could be considered sharing the same emotion cache between all the SSR requests to speed up performance.
   // However, be aware that it can have global side effects.
   const cache = createEmotionCache();
   const { extractCriticalToChunks } = createEmotionServer(cache);
